7b292cb36da9cf0d86312cba6b5ffa95adfe5bce
Dev <2019umt1842@mnit.ac.in>
2023-08-02 22:26:47 +0530

    backend deploy first try

---
+++ index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname,'/client/build')));
+
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname, 'client','build','index.html'));
+//   });
+// }
+// else{
+//   app.get("/",(req,res)=>{
+//     res.send("Api running");
+//   });
+// }
+
+const PORT =process.env.PORT;
+app.listen(PORT, () => {
+  connect();
+  console.log("Connected to backend.");
+});
